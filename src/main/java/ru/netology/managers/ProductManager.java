a869950ace335b7115efdba0a4237bc1e5009ee4
balrom1981 <balrom@bk.ru>
2021-02-06 17:51:58 +0300

    SearchBy added

---
+++ src/main/java/ru/netology/managers/ProductManager.java
@@ -0,0 +1,30 @@
+package ru.netology.managers;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class ProductManager {
+    private ProductRepository repository = new ProductRepository();
+
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product : repository.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                // используйте System.arraycopy, чтобы скопировать всё из result в tmp
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        // ваш код
+
+    }
+
+}
+
