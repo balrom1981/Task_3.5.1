f38cb99f553e107ed74ba18bdf859acebeab1b2f
balrom1981 <balrom@bk.ru>
2021-02-07 17:55:23 +0300

    Test added

--- src/test/java/ru/netology/managers/ProductManagerTest.java
+++ src/test/java/ru/netology/managers/ProductManagerTest.java
@@ -10,6 +10,7 @@ import ru.netology.domain.Product;
 import ru.netology.domain.Smartphone;
 import ru.netology.repository.ProductRepository;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,7 +24,7 @@ class ProductManagerTest {
     private Product first = new Book(1, "Firs", 100, "Gogl");
     private Product second = new Book(2, "Second", 200, "Pushkin");
     private Product third = new Book(3, "Third", 300, "Tolstoi");
-    private Product fourth = new Smartphone(4, "IponeX", 100000, "Apple");
+    private Product fourth = new Smartphone(4, "IphoneX", 100000, "Apple");
     private Product fifth = new Smartphone(5, "Galaxy", 900000, "Samsung");
     private Product sixth = new Smartphone(6, "Fusion", 5000, "Lenovo");
 
@@ -33,6 +34,10 @@ class ProductManagerTest {
         Product[] returned = new Product[]{first, second, third, fourth, fifth, sixth};
         doReturn(returned).when(repository).findAll();
 
+        Product[] actual = manager.searchBy("Pushkin");
+        Product[] expected = new Product[] {second};
+
+        assertArrayEquals(actual, expected);
 
     }
 }
